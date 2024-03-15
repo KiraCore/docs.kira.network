60284564dde124bbbac3eb849279804e18214cb1
kmlbgn <kamil.begny@gmail.com>
2024-03-15 22:40:40 +0800

    learn intro

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import EditThisPage from '@theme-original/EditThisPage';
 import Link from '@docusaurus/Link';
-import IconEdit from '@theme/Icon/Edit';
 import {ThemeClassNames} from '@docusaurus/theme-common';
 
 export default function EditThisPageWrapper(props) {
   return (
     <>
-      <EditThisPage {...props} /> or&nbsp;
+      <EditThisPage {...props} /> 
+      or&nbsp;
       <Link to='https://github.com/KiraCore/docs.kira.network/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.yaml&title=%5BFEATURE+REQUEST%5D' className={ThemeClassNames.common.editThisPage}>
-        make a suggestion
-    </Link>
+      make a suggestion
+      </Link>
     </>
   );
 }
