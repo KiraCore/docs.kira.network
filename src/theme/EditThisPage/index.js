8dfb67af72c9a5319cc118362baee79952c88fc4
kmlbgn <kamil.begny@gmail.com>
2024-03-16 10:24:32 +0800

    docs: ryokai, learn: intro

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -7,7 +7,7 @@ export default function EditThisPageWrapper(props) {
   return (
     <>
       <EditThisPage {...props} /> 
-      or&nbsp;
+      &nbsp; or &nbsp;
       <Link to='https://github.com/KiraCore/docs.kira.network/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.yaml&title=%5BFEATURE+REQUEST%5D' className={ThemeClassNames.common.editThisPage}>
       make a suggestion
       </Link>
