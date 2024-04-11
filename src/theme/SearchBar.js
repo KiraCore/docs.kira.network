a42533c14cb29a7bb166478597011725c97395f1
kmlbgn <kamil.begny@gmail.com>
2024-04-11 15:31:55 +0800

    cookbook API key

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -7,7 +7,7 @@ export default function SearchBarWrapper(props) {
     <>
       <SearchBar {...props} />
       <AskCookbook
-        apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWQ5OTljM2Q0NWUzNzIxM2UxZDgzMGMiLCJpYXQiOjE3MDg3NTk0OTEsImV4cCI6MjAyNDMzNTQ5MX0.2DgWnOpRQgPWfQeIOH5XRP3v1v_XDU5RSy6pqUUwwVg"
+        apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWVmNmUzMGIyMjE3MGZkZGNlNmZmMDciLCJpYXQiOjE3MTAxOTAxMjgsImV4cCI6MjAyNTc2NjEyOH0.8nv0LsYIESu0kFUabpQ-dGUGpJIHKjd8x_FjZy5LCB4"
       />
     </>
   );
